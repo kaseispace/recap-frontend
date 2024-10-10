95b4d71cb865246fa89969d7a03413c64ae55037
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:25:04 +0900

    feat: Formコンポーネントを作成

---
+++ src/components/Base/Form/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <!-- 大枠 -->
+  <form class="w-full max-w-[420px] rounded-sm border p-4 xs:p-8">
+    <!-- ヘッダー -->
+    <h2 class="border-b pb-5 text-center text-lg font-bold">
+      {{ title }}
+    </h2>
+
+    <!-- メイン -->
+    <div class="space-y-4 py-8">
+      <slot />
+    </div>
+
+    <!-- フッター -->
+    <slot name="footer" />
+  </form>
+</template>

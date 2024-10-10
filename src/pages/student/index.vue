b2bc325560aaf3eec00a1925bf6b5db1faab1a5b
yuki0829 <fledgling0829@gmail.com>
2024-10-10 21:02:20 +0900

    feat: 学生ページを作成

---
+++ src/pages/student/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['student-permissions'],
+  layout: 'student',
+})
+
+const { snackbarMessage, snackbarStatus } = useSnackBar()
+</script>
+
+<template>
+  <div>
+    <BaseSnackbar
+      :notification="snackbarMessage"
+      :status="snackbarStatus"
+    />
+    <div class="mx-auto my-16 w-full max-w-7xl space-y-16">
+      <div class="mx-6 flex flex-col xs:flex-row xs:items-center xs:justify-between">
+        <BaseTitle
+          class="mb-2 xs:mb-0"
+          text="受講中の授業一覧"
+        />
+
+        <DomainStudentIndexJoinCourseCard />
+      </div>
+
+      <div class="mx-6">
+        <DomainStudentIndexShowCourseCard />
+      </div>
+    </div>
+  </div>
+</template>

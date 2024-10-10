2c1fb9debb8cb1e681c4c7fec948ec98eadcb229
yuki0829 <fledgling0829@gmail.com>
2024-10-10 21:11:45 +0900

    feat: 非ログインページの共通レイアウトを作成

---
+++ src/layouts/default.vue
@@ -0,0 +1,26 @@
+<!-- 真ん中に配置したい場合はh-screenを追加 -->
+<template>
+  <div class="flex flex-col">
+    <BaseLayoutAppBar
+      school-name="ReCap"
+      title-font="font-bold"
+    >
+      <div class="flex items-center justify-center">
+        <BaseLink
+          text="ログイン"
+          path="index"
+          class="font-medium hover:text-sky-600"
+        />
+        <span class="mx-1.5">/</span>
+        <BaseLink
+          text="新規登録"
+          path="signup"
+          class="font-medium hover:text-sky-600"
+        />
+      </div>
+    </BaseLayoutAppBar>
+    <main class="flex flex-1 items-center justify-center overflow-y-auto py-10">
+      <slot />
+    </main>
+  </div>
+</template>

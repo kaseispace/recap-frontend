f6f865a55a39f9c26b76d635e0924b40da27caa1
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:30:24 +0900

    feat: Loadingコンポーネントを作成

---
+++ src/components/Base/Loading/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  iconSize?: 'small' | 'large'
+  borderColor: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  iconSize: 'large',
+})
+</script>
+
+<template>
+  <div
+    v-if="props.iconSize === 'small'"
+    data-testId="propsBorderColor"
+    class="size-8 animate-spin rounded-full border-4 border-t-transparent"
+    :class="[borderColor]"
+  />
+  <div
+    v-else
+    data-testId="propsBorderColor"
+    class="size-11 animate-spin rounded-full border-[6px] border-t-transparent"
+    :class="[borderColor]"
+  />
+</template>

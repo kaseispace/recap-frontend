19f577e3f463ad6e4a643943cdf0e262d16beeb9
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:27:57 +0900

    feat: NavIconコンポーネントを作成

---
+++ src/components/Base/Layout/NavIcon.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  iconName: string
+  pathName: string
+  activeBg: string
+  inactiveBg: string
+}
+const props = defineProps<Props>()
+const route = useRoute()
+
+const isActive = computed(() => route.name === props.pathName)
+</script>
+
+<template>
+  <NuxtLink
+    data-testId="link"
+    :to="{ name: pathName }"
+  >
+    <div
+      data-testId="propsBgColor"
+      class="group flex h-10 w-10 items-center justify-center rounded-md p-1"
+      :class="isActive ? [activeBg] : [inactiveBg]"
+    >
+      <slot />
+
+      <Icon
+        data-testId="icon"
+        :name="iconName"
+        size="24px"
+      />
+    </div>
+  </NuxtLink>
+</template>

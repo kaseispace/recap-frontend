3f83ad5996215acf12bc04c8605bac75463fafe2
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:28:44 +0900

    feat: Linkコンポーネントを作成

---
+++ src/components/Base/Link/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  path: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <p class="text-sm">
+    <NuxtLink :to="{ name: path }">
+      {{ text }}
+    </NuxtLink>
+  </p>
+</template>

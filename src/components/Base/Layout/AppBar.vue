32f26c3cf0eeacc83dc5e512d0b29570f56f0321
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:26:53 +0900

    feat: AppBarコンポーネントを作成

---
+++ src/components/Base/Layout/AppBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+  schoolName: string
+  titleFont?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="sticky top-0 z-20 flex h-12 items-center justify-between border-b bg-white px-5 py-2">
+    <div class="flex items-center">
+      <slot name="icon" />
+      <span
+        :class="titleFont ? titleFont : 'text-lg font-medium'"
+        data-testId="schoolName"
+      >{{ schoolName }}</span>
+    </div>
+    <div class="text-sm">
+      <slot />
+    </div>
+  </div>
+</template>

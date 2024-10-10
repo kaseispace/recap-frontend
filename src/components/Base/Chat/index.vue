b43e04faa0cbaa3c82721ab8b8f45235df393273
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:17:49 +0900

    feat: Chatコンポーネントを作成

---
+++ src/components/Base/Chat/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  bgColor: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex h-[570px] w-[370px] flex-col rounded-sm shadow-2xl">
+    <!-- header(高さを指定しないでタイトルの長さに応じて) -->
+    <div
+      data-testId="propsBgColor"
+      class="flex items-center justify-center rounded-t-sm p-3"
+      :class="[bgColor]"
+    >
+      <span class="font-semibold text-white">{{ title }}</span>
+    </div>
+    <slot />
+  </div>
+</template>

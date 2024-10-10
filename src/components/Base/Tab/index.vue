38ac0daf7c47576d043a41a671e85f87b660573a
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:32:19 +0900

    feat: Tabコンポーネントを作成

---
+++ src/components/Base/Tab/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  isActive: boolean
+  activeColor: string
+}
+defineProps<Props>()
+defineEmits(['clickEmit'])
+</script>
+
+<template>
+  <li @click="$emit('clickEmit')">
+    <a
+      href="#"
+      class="inline-block whitespace-nowrap border-b-2 p-3"
+      :class="[
+        isActive ? [activeColor] : 'border-transparent  text-gray-500 hover:border-gray-300 hover:text-gray-900',
+      ]"
+    >{{ text }}</a>
+  </li>
+</template>

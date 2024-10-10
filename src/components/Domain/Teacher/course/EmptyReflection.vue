61be9ff5b9749e787eef364317ab31373e52aa99
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:38:37 +0900

    feat: EmptyReflectionコンポーネントを作成

---
+++ src/components/Domain/Teacher/course/EmptyReflection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const imageLoaded = ref(false)
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <div class="bg-white">
+      <NuxtImg
+        preload
+        provider="cloudinary"
+        src="t-empty-reflection_azzydl.png"
+        sizes="150px xs2:200px"
+        alt="プロンプトがまだ存在しません"
+        @load="imageLoaded = true"
+      />
+    </div>
+
+    <p
+      v-if="imageLoaded"
+      class="mt-2 text-base"
+    >
+      まだ振り返りはありません
+    </p>
+    <p
+      v-if="imageLoaded"
+      class="mt-2 text-sm text-gray-500"
+    >
+      登録済みの振り返りがここに表示されます
+    </p>
+  </div>
+</template>

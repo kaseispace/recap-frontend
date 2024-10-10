db4f457f00080f1f361a9b78bb90f8bc5b4e4be3
yuki0829 <fledgling0829@gmail.com>
2024-10-10 21:24:58 +0900

    chore: ライブラリの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  srcDir: 'src/',
+  modules: ['@vueuse/nuxt', '@samk-dev/nuxt-vcalendar', '@nuxt/image', '@nuxt/icon', '@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  runtimeConfig: {
+    public: {
+      firebase: {
+        apiKey: '',
+        authDomain: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
+        recaptchaSiteKey: '',
+      },
+      devBackendUrl: '',
+    },
+  },
+  image: {
+    screens: {
+      xs2: 480,
+    },
+    cloudinary: {
+      baseURL: 'https://res.cloudinary.com/dbveryjmc/image/upload/',
+    },
+  },
+  css: ['~/assets/css/scrollbar.css'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })

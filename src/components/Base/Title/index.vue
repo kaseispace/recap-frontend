5cde80f13cd073122553adab26732fa8a60579d9
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:32:48 +0900

    feat: Titleコンポーネントを作成

---
+++ src/components/Base/Title/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <h1 class="text-xl sm:text-2xl">
+    {{ text }}
+  </h1>
+</template>

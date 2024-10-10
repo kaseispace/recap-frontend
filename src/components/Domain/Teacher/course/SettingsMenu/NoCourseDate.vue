3ba432b2b4a5ed36247d7a12a85d2b0b6ee96ed9
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:43:17 +0900

    feat: NoCourseDateコンポーネントを作成

---
+++ src/components/Domain/Teacher/course/SettingsMenu/NoCourseDate.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const isCourseCodeDialogOpen = ref(false)
+const isAddDialogOpen = ref(false)
+
+const closeCourseCodeDialog = () => (isCourseCodeDialogOpen.value = false)
+const closeAddDialog = () => (isAddDialogOpen.value = false)
+
+const handleMultipleActions = (actionId: number) => {
+  switch (actionId) {
+    case 1:
+      isCourseCodeDialogOpen.value = true
+      break
+    case 2:
+      isAddDialogOpen.value = true
+      break
+  }
+}
+</script>
+
+<template>
+  <div>
+    <BaseMore
+      data-testId="settingsMenu"
+      :menu-array="NoCourseDateSettings"
+      hover-bgcolor="hover:bg-slate-300/50"
+      :icon-color="false"
+      @select-menu="handleMultipleActions"
+    >
+      <Icon
+        name="mi:options-vertical"
+        size="20px"
+      />
+    </BaseMore>
+
+    <DomainTeacherCourseSettingsMenuCourseCodeDialog
+      v-if="isCourseCodeDialogOpen"
+      @dialog-closed="closeCourseCodeDialog"
+    />
+
+    <DomainTeacherCourseSettingsMenuAddDialog
+      v-if="isAddDialogOpen"
+      @dialog-closed="closeAddDialog"
+    />
+  </div>
+</template>

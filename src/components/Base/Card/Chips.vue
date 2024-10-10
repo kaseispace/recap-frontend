630c5b21302fd93d8a64c64095fdccbf9db9a31e
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:13:48 +0900

    feat: Chipsコンポーネントを作成

---
+++ src/components/Base/Card/Chips.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  iconType: 'calendar' | 'timetable'
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    data-testId="slotChips"
+    class="ml-2 flex"
+  >
+    <div class="mt-0.5 flex items-center">
+      <Icon
+        v-if="iconType === 'calendar'"
+        data-testId="calendar"
+        name="mdi:calendar-month-outline"
+        size="12"
+      />
+      <Icon
+        v-else-if="iconType === 'timetable'"
+        data-testId="timetable"
+        name="mdi:timetable"
+        size="12"
+      />
+    </div>
+    <p class="text-xs">
+      {{ text }}
+    </p>
+  </div>
+</template>

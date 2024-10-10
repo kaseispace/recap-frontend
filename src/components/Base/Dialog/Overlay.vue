d7557b157614ebd1c9f78029d3b261aaa016f2d1
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:23:13 +0900

    feat: Overlayコンポーネントを作成

---
+++ src/components/Base/Dialog/Overlay.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+  dialogType?: 'right' | 'left' | 'center'
+}
+
+withDefaults(defineProps<Props>(), {
+  dialogType: 'center',
+})
+</script>
+
+<template>
+  <div
+    :class="{
+      'justify-end': dialogType === 'right',
+      'justify-start': dialogType === 'left',
+      'items-center justify-center': dialogType === 'center',
+    }"
+    class="fixed left-0 top-0 z-20 flex size-full bg-slate-500/50"
+  >
+    <slot />
+  </div>
+</template>

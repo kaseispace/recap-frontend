f6c1d938c6aca03e567277ab0c2b843cd6806ad0
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:42:03 +0900

    feat: CourseDateAndReflectionAddedコンポーネントを作成

---
+++ src/components/Domain/Teacher/course/SettingsMenu/CourseDateAndReflectionAdded.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const isCourseCodeDialogOpen = ref(false)
+const isDeleteDialogOpen = ref(false)
+
+const closeCourseCodeDialog = () => (isCourseCodeDialogOpen.value = false)
+const closeDeleteDialog = () => (isDeleteDialogOpen.value = false)
+
+const handleMultipleActions = (actionId: number) => {
+  switch (actionId) {
+    case 1:
+      isCourseCodeDialogOpen.value = true
+      break
+    case 2:
+      isDeleteDialogOpen.value = true
+      break
+  }
+}
+</script>
+
+<template>
+  <div>
+    <BaseMore
+      data-testId="settingsMenu"
+      :menu-array="CourseDateAndReflectionAddedSettings"
+      hover-bgcolor="hover:bg-slate-300/50"
+      :icon-color="false"
+      @select-menu="handleMultipleActions"
+    >
+      <Icon
+        name="mi:options-vertical"
+        size="20px"
+      />
+    </BaseMore>
+
+    <DomainTeacherCourseSettingsMenuCourseCodeDialog
+      v-if="isCourseCodeDialogOpen"
+      @dialog-closed="closeCourseCodeDialog"
+    />
+    <DomainTeacherCourseSettingsMenuDeleteDialog
+      v-if="isDeleteDialogOpen"
+      @dialog-closed="closeDeleteDialog"
+    />
+  </div>
+</template>

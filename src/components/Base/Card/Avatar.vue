b20a5ad3360e5c241a50b401e177aaca8b5a13dd
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:13:11 +0900

    feat: Avatarコンポーネントを作成

---
+++ src/components/Base/Card/Avatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  bgColor: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="mr-1 flex items-center justify-center rounded-full p-2"
+    :class="[bgColor]"
+  >
+    <slot />
+  </div>
+</template>

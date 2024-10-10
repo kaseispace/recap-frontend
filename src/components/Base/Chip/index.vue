04587287f2a8a88116e7593d7626cf963d1d56bc
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:19:56 +0900

    feat: Chipコンポーネントを作成

---
+++ src/components/Base/Chip/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  isActive: boolean
+  activeColor: string
+}
+defineProps<Props>()
+defineEmits(['clickEmit'])
+</script>
+
+<template>
+  <button
+    class="mb-2 mr-2 flex items-center justify-center rounded-full px-3 py-[5px] text-xs"
+    :class="[isActive ? [activeColor] : 'bg-gray-100 text-black hover:bg-gray-200']"
+    @click="$emit('clickEmit')"
+  >
+    {{ text }}
+  </button>
+</template>

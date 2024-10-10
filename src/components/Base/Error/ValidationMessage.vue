cf6a268dae6e6cde8d94c86722cc0df11e20749d
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:24:09 +0900

    feat: ValidationMessageコンポーネントを作成

---
+++ src/components/Base/Error/ValidationMessage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <p class="mt-1 text-sm font-medium text-red-600">
+    {{ text }}
+  </p>
+</template>

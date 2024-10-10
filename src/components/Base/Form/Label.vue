13718d5ffe50411168e534c0a7580e466ed9331d
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:25:18 +0900

    feat: Labelコンポーネントを作成

---
+++ src/components/Base/Form/Label.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  inputFor?: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <label
+    :for="inputFor"
+    class="mb-2 block text-sm font-semibold text-gray-900"
+  >{{ text }}</label>
+</template>

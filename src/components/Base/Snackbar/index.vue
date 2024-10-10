775338bd775f91c665cfe33db50e1fc36adc5c9c
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:32:06 +0900

    feat: Snackbarコンポーネントを作成

---
+++ src/components/Base/Snackbar/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+interface Props {
+  notification: ValueText | null
+  status: boolean | null
+}
+const props = defineProps<Props>()
+const isSnackbarVisible = ref(false)
+const iconStyle = props.status ? 'true' : 'false'
+
+watch(
+  () => props.notification,
+  (): void => {
+    isSnackbarVisible.value = true
+    setTimeout(() => {
+      isSnackbarVisible.value = false
+    }, 5000)
+  },
+)
+</script>
+
+<template>
+  <div
+    v-if="isSnackbarVisible"
+    data-testId="invisible"
+    class="fixed right-2 top-2 z-30"
+  >
+    <div
+      data-testId="propsStatus"
+      class="flex items-center rounded-lg px-5 py-3"
+      :class="[status ? 'bg-teal-900' : 'bg-red-500']"
+    >
+      <Icon
+        v-if="!iconStyle"
+        name="mdi:alert-circle"
+        size="20px"
+        style="color: white"
+      />
+      <Icon
+        v-else
+        name="mdi:check-circle"
+        size="20px"
+        style="color: white"
+      />
+      <p class="ml-2 text-sm text-white">
+        {{ props.notification?.text }}
+      </p>
+    </div>
+  </div>
+</template>

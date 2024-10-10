a2f88c45edb53bed5a93e4cb7cd46677e66db1a8
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:15:55 +0900

    feat: Reflectionコンポーネントを作成

---
+++ src/components/Base/Card/Reflection.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  date: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="rounded-lg border border-gray-200 bg-white p-5 shadow">
+    <div class="mb-4 flex flex-col justify-start border-b border-gray-200 pb-3">
+      <h1 class="mb-1 text-xl">
+        {{ title }}
+      </h1>
+      <p class="text-sm text-gray-500">
+        {{ date }}
+      </p>
+    </div>
+    <slot />
+  </div>
+</template>

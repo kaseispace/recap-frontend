be086c96f1aff0b29d7ebeaae181430cb0e3be2a
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:59:41 +0900

    feat: errorページを作成

---
+++ src/error.vue
@@ -0,0 +1,183 @@
+<script setup lang="ts">
+interface Props {
+  error: {
+    statusCode?: number
+  }
+}
+const props = defineProps<Props>()
+
+const isResetDialogOpen = ref(false)
+const isDeleteDialogOpen = ref(false)
+const isImageLoaded = ref(false)
+
+const { authUser } = useAuth()
+const { userInfo } = useUserApi()
+const { teacherCourses, getCourse, isTeacherCourseLoading } = useCourseApi()
+const { studentCourses, getJoinedCourses, isStudentCourseLoading } = useUserCourseApi()
+const { snackbarMessage, snackbarStatus } = useSnackBar()
+const closeResetDialog = () => (isResetDialogOpen.value = false)
+const closeDeleteDialog = () => (isDeleteDialogOpen.value = false)
+const imageLoad = (loaded: boolean) => (isImageLoaded.value = loaded)
+
+const goToRootPage = async () => await navigateTo('/')
+
+const handleEditOrDeleteAction = (actionId: number) => {
+  switch (actionId) {
+    case 1:
+      isResetDialogOpen.value = true
+      break
+    case 2:
+      isDeleteDialogOpen.value = true
+      break
+  }
+}
+
+onMounted(async () => {
+  if (authUser.value) {
+    if (userInfo.value?.user.user_type === 1 && !teacherCourses.value) {
+      const idToken = await authUser.value.getIdToken()
+      teacherCourses.value = await getCourse(idToken)
+      isTeacherCourseLoading.value = false
+    }
+    else if (userInfo.value?.user.user_type === 0 && !studentCourses.value) {
+      const idToken = await authUser.value.getIdToken()
+      studentCourses.value = await getJoinedCourses(idToken)
+      isStudentCourseLoading.value = false
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="flex h-full">
+    <BaseSnackbar
+      :notification="snackbarMessage"
+      :status="snackbarStatus"
+    />
+    <!-- ログイン済みの共通サイドバー -->
+    <BaseLayoutSideBar
+      v-if="userInfo"
+      :bg-color="teacherCourses ? 'hover:bg-blue-600/10' : 'hover:bg-cyan-600/10'"
+    >
+      <BaseLayoutNavIcon
+        icon-name="fluent-mdl2:home"
+        :path-name="teacherCourses ? 'teacher' : 'student'"
+        :active-bg="teacherCourses ? 'bg-blue-600/40 hover:bg-blue-600/40' : 'bg-cyan-600/40 hover:bg-cyan-600/40'"
+        :inactive-bg="teacherCourses ? 'hover:bg-blue-600/10' : 'hover:bg-cyan-600/10'"
+      >
+        <BaseTooltip
+          class="left-[60px]"
+          message="ホーム"
+        />
+      </BaseLayoutNavIcon>
+
+      <BaseLayoutIconMenuList
+        :courses="teacherCourses ? teacherCourses : studentCourses"
+        :title="teacherCourses ? '担当授業一覧' : '受講一覧'"
+        :course-name="teacherCourses ? TeacherCourseDetail : StudentCourseDetail"
+        :active-bg="teacherCourses ? 'bg-blue-600/40' : 'bg-cyan-600/40'"
+        :list-active-bg="teacherCourses ? 'bg-blue-600/10 text-blue-900' : 'bg-cyan-600/10 text-cyan-900'"
+        :inactive-bg="teacherCourses ? 'hover:bg-blue-600/10' : 'hover:bg-cyan-600/10'"
+        list-inactive-bg="hover:bg-slate-100 text-gray-500 hover:text-black"
+      >
+        <Icon
+          name="streamline:class-lesson"
+          size="24px"
+        />
+        <BaseTooltip
+          class="left-[52px]"
+          message="クラス"
+        />
+      </BaseLayoutIconMenuList>
+    </BaseLayoutSideBar>
+
+    <main class="flex w-full flex-1 flex-col">
+      <!-- アプリケーションバー -->
+      <BaseLayoutAppBar :school-name="userInfo ? userInfo.school.name : 'LMS'">
+        <!-- エラーの際もユーザーアイコンを表示させるかどうか -->
+
+        <template
+          v-if="userInfo"
+          #icon
+        >
+          <BaseLayoutMobileSideBar
+            :courses="teacherCourses ? teacherCourses : studentCourses"
+            :title="teacherCourses ? '担当授業一覧' : '受講一覧'"
+            :course-name="teacherCourses ? TeacherCourseDetail : StudentCourseDetail"
+            :list-active-bg="teacherCourses ? 'bg-blue-600/10 text-blue-900' : 'bg-cyan-600/10 text-cyan-900'"
+            list-inactive-bg="hover:bg-slate-100 text-gray-500 hover:text-black"
+            :path-name="teacherCourses ? 'teacher' : 'student'"
+          />
+        </template>
+
+        <BaseMoreAccount
+          v-if="authUser?.email && userInfo"
+          :name="userInfo.user.name"
+          :email="authUser.email"
+          :role="userInfo.user.user_type"
+          @select-menu="handleEditOrDeleteAction"
+        />
+
+        <div
+          v-else
+          class="flex items-center justify-center"
+        >
+          <BaseLink
+            text="ログイン"
+            path="index"
+            class="font-medium hover:text-sky-600"
+          />
+          <span class="mx-1.5">/</span>
+          <BaseLink
+            text="新規登録"
+            path="signup"
+            class="font-medium hover:text-sky-600"
+          />
+        </div>
+      </BaseLayoutAppBar>
+      <div class="flex-1">
+        <DomainPasswordResetDialog
+          v-if="isResetDialogOpen"
+          :bg-color="
+            teacherCourses
+              ? 'bg-blue-600 text-white hover:bg-blue-600/75'
+              : 'bg-cyan-600 text-white hover:bg-cyan-600/75'
+          "
+          @dialog-closed="closeResetDialog"
+        />
+        <DomainAccountDeletionDialog
+          v-if="isDeleteDialogOpen"
+          @dialog-closed="closeDeleteDialog"
+        />
+
+        <div class="mt-[80px] flex flex-col items-center px-4 sm:mt-[140px]">
+          <template v-if="props.error.statusCode === 404">
+            <BaseError404
+              v-if="userInfo?.user.user_type === 1"
+              img-path="t-404_jngqh1.png"
+              @image-load="imageLoad"
+            />
+            <BaseError404
+              v-else-if="userInfo?.user.user_type === 0"
+              img-path="s-404_eadka6.png"
+              @image-load="imageLoad"
+            />
+            <BaseError404
+              v-else
+              img-path="404_n72mzp.png"
+              @image-load="imageLoad"
+            />
+          </template>
+
+          <BaseButton
+            v-if="isImageLoaded"
+            text="トップページへ戻る"
+            button-type="circle"
+            class="border px-3 py-2 hover:bg-gray-200"
+            @click="goToRootPage"
+          />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>

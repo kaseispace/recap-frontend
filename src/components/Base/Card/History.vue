6ba065384fc6cfdb38b3fd315a8439d7637ef81b
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:14:38 +0900

    feat: Historyコンポーネントを作成

---
+++ src/components/Base/Card/History.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  bgColor: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    data-testId="propsBgColor"
+    class="rounded-lg p-5"
+    :class="[bgColor]"
+  >
+    <div class="mb-5 flex flex-col justify-start">
+      <h1 class="text-xl">
+        {{ title }}
+      </h1>
+    </div>
+    <slot />
+  </div>
+</template>

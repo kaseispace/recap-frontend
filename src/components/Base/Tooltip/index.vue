99ec5d48138c830729da055446ccfb483101c6fe
yuki0829 <fledgling0829@gmail.com>
2024-10-10 20:33:04 +0900

    feat: Tooltipコンポーネントを作成

---
+++ src/components/Base/Tooltip/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  message: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <span
+    class="pointer-events-none absolute whitespace-nowrap rounded bg-gray-700 px-2 py-1 text-[11px] text-white opacity-0 transition group-hover:opacity-100"
+  >{{ message }}</span>
+</template>

4db2796d09af7945e1dd5c56de718a238a278bd0
yuki0829 <fledgling0829@gmail.com>
2024-10-10 21:02:35 +0900

    feat: 教員ページを作成

---
+++ src/pages/teacher/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['teacher-permissions'],
+  layout: 'teacher',
+})
+
+const { snackbarMessage, snackbarStatus } = useSnackBar()
+</script>
+
+<template>
+  <div>
+    <BaseSnackbar
+      :notification="snackbarMessage"
+      :status="snackbarStatus"
+    />
+    <div class="mx-auto my-16 w-full max-w-7xl space-y-16">
+      <div class="mx-6 flex flex-col xs:flex-row xs:items-center xs:justify-between">
+        <BaseTitle
+          class="mb-2 xs:mb-0"
+          text="担当授業一覧"
+        />
+
+        <DomainTeacherIndexCreateCourseCard />
+      </div>
+
+      <div class="mx-6">
+        <DomainTeacherIndexShowCourseCard />
+      </div>
+    </div>
+  </div>
+</template>

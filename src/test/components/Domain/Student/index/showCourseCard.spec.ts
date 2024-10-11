ba15b5ff004c03106f2d544b1ae915be33e5546c
yuki0829 <fledgling0829@gmail.com>
2024-10-11 20:37:13 +0900

    test: ShowCourseCardコンポーネントのテストを作成

---
+++ src/test/components/Domain/Student/index/showCourseCard.spec.ts
@@ -0,0 +1,93 @@
+// @vitest-environment nuxt
+import { mockComponent, mountSuspended } from '@nuxt/test-utils/runtime'
+import ShowCourseCard from '@/components/Domain/Student/index/ShowCourseCard.vue'
+import { mockAuthUser, wait } from '@/test/mocks/index'
+import { registerUserCourseEndpoints } from '@/test/mocks/userCourse/endpoints'
+import { mockStudentCourseData } from '@/test/mocks/course/index'
+
+mockComponent('BaseEmpty', {
+  template: '<div data-testId="empty">stub empty course</div>'
+})
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    handleLeaveAction: (_actionId: number, course: StudentCourse) => void
+  }
+}
+
+describe('ShowCourseCardコンポーネントのテスト', () => {
+  let auth: AuthReturnType
+  let userCourse: UserCourseReturnType
+  registerUserCourseEndpoints()
+
+  beforeEach(() => {
+    auth = useAuth()
+    userCourse = useUserCourseApi()
+  })
+
+  afterEach(() => {
+    auth.authUser.value = null
+    userCourse.studentCourses.value = null
+    userCourse.isStudentCourseLoading.value = true
+  })
+
+  describe('受講中の授業が存在しない場合', () => {
+    it('授業がないことを示すコンポーネントが表示される', async () => {
+      const wrapper = await mountSuspended(ShowCourseCard)
+      userCourse.studentCourses.value = []
+      userCourse.isStudentCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.find(`[data-testId="empty"]`).exists()).toBe(true)
+    })
+  })
+
+  describe('受講中の授業が存在する場合', () => {
+    it('授業カードが表示される', async () => {
+      const wrapper = await mountSuspended(ShowCourseCard)
+      userCourse.studentCourses.value = mockStudentCourseData
+      userCourse.isStudentCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(userCourse.studentCourses.value).toBeTruthy()
+
+      if (!userCourse.studentCourses.value) return
+
+      const cardHref = wrapper.find(`[data-testId="card-0"]`).get('a').attributes('href')
+
+      expect(cardHref).toBe(`/student/course/${userCourse.studentCourses.value[0].uuid}`)
+
+      const courseCard = wrapper.find(`[data-testId="card-0"]`).text()
+
+      expect(courseCard).toContain(userCourse.studentCourses.value[0].teacher_name)
+      expect(courseCard).toContain(userCourse.studentCourses.value[0].course_time)
+      expect(courseCard).toContain(userCourse.studentCourses.value[0].day_of_week)
+    })
+
+    it('選択した授業から退出することができる', async () => {
+      auth.authUser.value = mockAuthUser
+      const wrapper = await mountSuspended(ShowCourseCard)
+      userCourse.studentCourses.value = mockStudentCourseData
+      userCourse.isStudentCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(userCourse.studentCourses.value).toBeTruthy()
+
+      if (!userCourse.studentCourses.value) return
+
+      wrapper.vm.handleLeaveAction(0, userCourse.studentCourses.value[0])
+
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.find(`[data-testId="leaveDialog"]`).exists()).toBe(true)
+
+      await wrapper.find(`[data-testId="clickLeave"]`).trigger('click')
+      await wait(100)
+
+      expect(userCourse.studentCourses.value.length).toBe(0)
+    })
+  })
+})
---
+++ src/test/components/Domain/Teacher/index/showCourseCard.spec.ts
@@ -0,0 +1,121 @@
+// @vitest-environment nuxt
+import { mockComponent, mountSuspended } from '@nuxt/test-utils/runtime'
+import ShowCourseCard from '@/components/Domain/Teacher/index/ShowCourseCard.vue'
+import { mockAuthUser, wait } from '@/test/mocks/index'
+import { registerCourseEndpoints } from '@/test/mocks/course/endpoints'
+import { mockCoursesData } from '@/test/mocks/course/index'
+
+mockComponent('BaseEmpty', {
+  template: '<div data-testId="empty">stub empty course</div>'
+})
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    handleEditOrDeleteAction: (actionId: number, course: Course) => void
+  }
+}
+
+describe('ShowCourseCardコンポーネントのテスト', () => {
+  let auth: AuthReturnType
+  let course: CourseApiReturnType
+  registerCourseEndpoints()
+
+  beforeEach(() => {
+    auth = useAuth()
+    course = useCourseApi()
+  })
+
+  afterEach(() => {
+    auth.authUser.value = null
+    course.teacherCourses.value = null
+    course.isTeacherCourseLoading.value = true
+  })
+
+  describe('担当授業が存在しない場合', () => {
+    it('授業がないことを示すコンポーネントが表示される', async () => {
+      const wrapper = await mountSuspended(ShowCourseCard)
+      course.teacherCourses.value = []
+      course.isTeacherCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.find(`[data-testId="empty"]`).exists()).toBe(true)
+    })
+  })
+
+  describe('担当授業が存在する場合', () => {
+    it('授業カードが表示される', async () => {
+      const wrapper = await mountSuspended(ShowCourseCard)
+      course.teacherCourses.value = mockCoursesData
+      course.isTeacherCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(course.teacherCourses.value).toBeTruthy()
+
+      if (!course.teacherCourses.value) return
+
+      for (let i = 0; i < course.teacherCourses.value.length; i++) {
+        const cardHref = wrapper.find(`[data-testId="card-${i}"]`).get('a').attributes('href')
+
+        expect(cardHref).toBe(`/teacher/course/${course.teacherCourses.value[i].uuid}`)
+
+        const courseCard = wrapper.find(`[data-testId="card-${i}"]`).text()
+
+        expect(courseCard).toContain(course.teacherCourses.value[i].course_time)
+        expect(courseCard).toContain(course.teacherCourses.value[i].day_of_week)
+      }
+    })
+
+    it('編集用ダイアログが表示され、授業の編集ができる', async () => {
+      auth.authUser.value = mockAuthUser
+      const wrapper = await mountSuspended(ShowCourseCard)
+
+      course.teacherCourses.value = mockCoursesData
+      course.isTeacherCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(course.teacherCourses.value).toBeTruthy()
+
+      if (!course.teacherCourses.value) return
+
+      wrapper.vm.handleEditOrDeleteAction(1, course.teacherCourses.value[0])
+
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.find(`[data-testId="editDialog"]`).exists()).toBe(true)
+
+      await wrapper.find(`[data-testId="name"]`).setValue('データサイエンス入門')
+      await wrapper.find(`[data-testId="clickEdit"]`).trigger('click')
+      await wait(100)
+
+      expect(course.teacherCourses.value[0].name).toBe('データサイエンス入門')
+    })
+
+    it('削除用ダイアログが表示され、授業を削除できる', async () => {
+      auth.authUser.value = mockAuthUser
+      const wrapper = await mountSuspended(ShowCourseCard)
+
+      course.teacherCourses.value = mockCoursesData
+      course.isTeacherCourseLoading.value = false
+
+      await wrapper.vm.$nextTick()
+
+      expect(course.teacherCourses.value).toBeTruthy()
+
+      if (!course.teacherCourses.value) return
+
+      wrapper.vm.handleEditOrDeleteAction(2, course.teacherCourses.value[0])
+
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.find(`[data-testId="deleteDialog"]`).exists()).toBe(true)
+
+      await wrapper.find(`[data-testId="clickDelete"]`).trigger('click')
+      await wait(100)
+
+      expect(course.teacherCourses.value).not.toEqual(mockCoursesData[0])
+    })
+  })
+})

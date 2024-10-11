51d338ed2ea88782ed84894ad1087efad7e5b989
yuki0829 <fledgling0829@gmail.com>
2024-10-11 21:00:09 +0900

    feat: userApiのモックを作成

---
+++ src/test/mocks/user/endpoints.ts
@@ -0,0 +1,18 @@
+import { registerEndpoint } from '@nuxt/test-utils/runtime'
+import { mockAuthUser } from '@/test/mocks/index'
+import { mockCurrentUserData } from '@/test/mocks/user/index'
+
+const config = useRuntimeConfig()
+
+export const registerUserEndpoints = () => {
+  // ユーザーの削除
+  registerEndpoint(`${config.public.devBackendUrl}/users/${mockAuthUser.uid}`, {
+    method: 'DELETE',
+    handler: () => {
+      const deleteUser = mockCurrentUserData.find(user => user.uid === mockAuthUser.uid)
+      if (deleteUser) {
+        return {}
+      }
+    }
+  })
+}

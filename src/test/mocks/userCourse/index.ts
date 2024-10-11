d34849703cb23a8eaa5fa18ce591464f83b65db0
yuki0829 <fledgling0829@gmail.com>
2024-10-11 21:02:06 +0900

    feat: userCourseApiのmockデータを作成

---
+++ src/test/mocks/userCourse/index.ts
@@ -0,0 +1,17 @@
+export interface userCourseRequestBody {
+  school_id: number
+  course_code: string
+}
+
+export interface userCourse {
+  user_id: number
+  course_id: number
+}
+
+export const mockUserCoursesData = [
+  { id: 1, user_id: 1, course_id: 1 },
+  { id: 1, user_id: 1, course_id: 2 },
+  { id: 1, user_id: 1, course_id: 3 },
+  { id: 1, user_id: 1, course_id: 4 },
+  { id: 1, user_id: 1, course_id: 5 }
+]

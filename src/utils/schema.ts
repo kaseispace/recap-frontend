29e62a3369cbd53a52a3529e568f3e0dc234cdec
yuki0829 <fledgling0829@gmail.com>
2024-10-21 10:32:29 +0900

    feat: trim()を追加

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -7,7 +7,7 @@ export const LoginFormSchema = yup.object({
 })
 
 export const SignUpFormSchema = yup.object({
-  name: yup.string().required('必須項目です'),
+  name: yup.string().trim().required('必須項目です'),
   email: yup.string().required('必須項目です').email('メールアドレスの形式で入力してください'),
   password: yup.string().required('必須項目です').min(8, '8文字以上で入力してください'),
   passwordConfirm: yup
@@ -15,18 +15,18 @@ export const SignUpFormSchema = yup.object({
     .required('必須項目です')
     .oneOf([yup.ref('password')], 'パスワードが一致しません'),
   roleText: yup.string().required('必須項目です'),
-  schoolName: yup.string().required('必須項目です')
+  schoolName: yup.string().trim().required('必須項目です')
 })
 
 export const CourseFormSchema = yup.object({
-  name: yup.string().required('必須項目です'),
-  teacherName: yup.string().required('必須項目です'),
+  name: yup.string().trim().required('必須項目です'),
+  teacherName: yup.string().trim().required('必須項目です'),
   dayOfWeek: yup.string().required('必須項目です'),
   courseTime: yup.string().required('必須項目です')
 })
 
 export const CourseDateSchema = yup.object({
-  courseSession: yup.string().required('必須項目です').matches(/^\d+$/, '半角数字を入力してください'),
+  courseSession: yup.string().trim().required('必須項目です').matches(/^\d+$/, '半角数字を入力してください'),
   courseDate: yup.string().required('必須項目です')
 })
 
@@ -35,7 +35,7 @@ export const CourseCodeSchema = yup.object({
 })
 
 export const AnnouncementSchema = yup.object({
-  content: yup.string().required('必須項目です')
+  content: yup.string().trim().required('必須項目です')
 })
 
 export const PromptSchema = yup.object({
@@ -44,7 +44,7 @@ export const PromptSchema = yup.object({
 })
 
 export const ReflectionSchema = yup.object({
-  message: yup.string().required('必須項目です')
+  message: yup.string().trim().required('必須項目です')
 })
 
 export const PasswordResetSchema = yup.object({

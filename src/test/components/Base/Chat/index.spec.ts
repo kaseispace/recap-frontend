7a07c214e0323a8c7d6c7fcf17c22bb20319bcbe
yuki0829 <fledgling0829@gmail.com>
2024-10-25 11:10:22 +0900

    test: iconスロットのテストを追加

--- src/test/components/Base/Chat/index.spec.ts
+++ src/test/components/Base/Chat/index.spec.ts
@@ -1,14 +1,32 @@
 // @vitest-environment nuxt
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import Chat from '@/components/Base/Chat/index.vue'
+import Button from '@/components/Base/Button/index.vue'
 
 describe('Chatコンポーネントのテスト', () => {
-  it('propsでtitleに「テストチャット」が渡された場合、正しく表示され、背景色が「bg-cyan-800」で表示される', async () => {
+  it('propsが正しく渡される', async () => {
     const wrapper = await mountSuspended(Chat, {
-      props: { title: 'テストチャット', bgColor: 'bg-cyan-800' }
+      props: { title: 'テストチャット', bgColor: 'bg-cyan-800', isExpanded: false }
     })
 
     expect(wrapper.get('span').text()).toBe('テストチャット')
     expect(wrapper.find(`[data-testId="propsBgColor"]`).classes()).toContain('bg-cyan-800')
+    expect(wrapper.find(`[data-testId="propsIsExpanded"]`).classes()).toEqual(
+      expect.arrayContaining(['h-[570px]', 'w-[370px]', 'max-w-[80vw]'])
+    )
+    expect(wrapper.find(`[data-testId="propsIsExpanded"]`).classes()).not.toEqual(
+      expect.arrayContaining(['h-[90vh]', 'w-[90vw]', 'max-w-screen-xl'])
+    )
+  })
+
+  it('iconスロットにButtonコンポーネントを渡すと、正しくレンダリングされる', async () => {
+    const wrapper = await mountSuspended(Chat, {
+      props: { title: 'テストチャット', bgColor: 'bg-cyan-800', isExpanded: false },
+      slots: {
+        icon: () => h(Button, { buttonType: 'circle' })
+      }
+    })
+
+    expect(wrapper.find('button').exists()).toBe(true)
   })
 })

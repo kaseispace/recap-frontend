ccc93cb8263563d24e29ab40a41cab5da071f1d4
yuki0829 <fledgling0829@gmail.com>
2024-10-25 11:04:41 +0900

    feat: isExpandedプロパティを追加し、レイアウトを調整

--- src/components/Base/Chat/index.vue
+++ src/components/Base/Chat/index.vue
@@ -2,19 +2,25 @@
 interface Props {
   title: string
   bgColor: string
+  isExpanded: boolean
 }
 defineProps<Props>()
 </script>
 
 <template>
-  <div class="flex h-[570px] w-[370px] flex-col rounded-sm shadow-2xl">
+  <div
+    data-testId="propsIsExpanded"
+    class="flex flex-col rounded-sm shadow-2xl"
+    :class="isExpanded ? 'h-[90vh] w-[90vw] max-w-screen-xl' : 'h-[570px] w-[370px] max-w-[80vw]'"
+  >
     <!-- header(高さを指定しないでタイトルの長さに応じて) -->
     <div
       data-testId="propsBgColor"
-      class="flex items-center justify-center rounded-t-sm p-3"
+      class="flex items-center justify-between rounded-t-sm p-3"
       :class="[bgColor]"
     >
       <span class="font-semibold text-white">{{ title }}</span>
+      <slot name="icon" />
     </div>
     <slot />
   </div>

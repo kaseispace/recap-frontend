82516c05dc1c8370f9a1155b2e935f84feec8699
yuki0829 <fledgling0829@gmail.com>
2024-10-25 11:05:43 +0900

    style: z-indexをz-20からz-30に変更

--- src/components/Base/Dialog/Overlay.vue
+++ src/components/Base/Dialog/Overlay.vue
@@ -15,7 +15,7 @@ withDefaults(defineProps<Props>(), {
       'justify-start': dialogType === 'left',
       'items-center justify-center': dialogType === 'center',
     }"
-    class="fixed left-0 top-0 z-20 flex size-full bg-slate-500/50"
+    class="fixed left-0 top-0 z-30 flex size-full bg-slate-500/50"
   >
     <slot />
   </div>

665055e4fa74606eea3ebe85833dfc0353b55340
yuki0829 <fledgling0829@gmail.com>
2024-11-05 20:55:16 +0900

    chore: ローディング画面の追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
+  spaLoadingTemplate: './spa-loading-template.html',
   srcDir: 'src/',
   modules: [
     '@vueuse/nuxt',

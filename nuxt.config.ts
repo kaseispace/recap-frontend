31fb9aec17456935cf8ccbfeb43fbbbb423e0b57
yuki0829 <fledgling0829@gmail.com>
2024-11-28 20:39:12 +0900

    chore: metaタグの修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
           content: 'ReCapは、教員と学生のための振り返り学習プラットフォームです。学びを深め、新たな気づきを得るためのサポートを提供します。' },
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: 'https://www.recaplearning.com' },
-        { property: 'og:image', content: 'https://res.cloudinary.com/dbveryjmc/image/upload/v1732688216/OGP_gm1tok.png' },
+        { property: 'og:image', content: 'https://res.cloudinary.com/dbveryjmc/image/upload/v1732793669/zsexdrcftvgyhjbun_nyqnnb.png' },
         { property: 'og:site_name', content: 'ReCap' },
         { property: 'og:locale', content: 'ja_JP' }
       ],

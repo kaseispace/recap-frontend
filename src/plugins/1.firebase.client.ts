f8fcefd776c2cb0c58fbff9bf0bf206daa05f1b5
yuki0829 <fledgling0829@gmail.com>
2024-11-30 11:31:04 +0900

    chore: 不要なコメントを削除

--- src/plugins/1.firebase.client.ts
+++ src/plugins/1.firebase.client.ts
@@ -1,16 +1,10 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-// import { initializeAppCheck, ReCaptchaV3Provider, getToken } from 'firebase/app-check'
-
-// declare global {
-//   // eslint-disable-next-line
-//   var FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string | undefined
-// }
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
   const { firebase } = config.public
-  // const app = initializeApp({ ...firebase })
+
   const firebaseConfig = {
     apiKey: firebase.apiKey,
     authDomain: firebase.authDomain,
@@ -22,20 +16,6 @@ export default defineNuxtPlugin(() => {
 
   const app = initializeApp(firebaseConfig)
   const auth = getAuth(app)
-
-  // if (config.public.dev) window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
-  // const appCheck = initializeAppCheck(app, {
-  //   provider: new ReCaptchaV3Provider(config.public.firebase.recaptchaSiteKey),
-  //   isTokenAutoRefreshEnabled: true
-  // })
-
-  // getToken(appCheck)
-  //   .then(() => {
-  //     console.log('AppCheck:Success')
-  //   })
-  //   .catch((error) => {
-  //     console.log(error.message)
-  //   })
 
   return {
     provide: {

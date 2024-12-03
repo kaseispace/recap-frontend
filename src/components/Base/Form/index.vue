9a48dd014a8f103bb577e65896a0ae3e71a11bb5
yuki0829 <fledgling0829@gmail.com>
2024-12-03 20:14:08 +0900

    style: 背景色を変更

--- src/components/Base/Form/index.vue
+++ src/components/Base/Form/index.vue
@@ -7,9 +7,9 @@ defineProps<Props>()
 
 <template>
   <!-- 大枠 -->
-  <form class="w-full max-w-[420px] rounded-sm border p-4 xs:p-8">
+  <form class="w-full max-w-[420px] rounded-lg border bg-slate-100 p-4 xs:p-8">
     <!-- ヘッダー -->
-    <h2 class="border-b pb-5 text-center text-lg font-bold">
+    <h2 class="border-b border-slate-300 pb-5 text-center text-lg font-bold">
       {{ title }}
     </h2>
 

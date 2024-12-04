2eaa18473bb81b53c929430a4df41f2b8af44e70
yuki0829 <fledgling0829@gmail.com>
2024-12-04 15:38:07 +0900

    test: propsにisEditedを追加

--- src/test/components/Base/Card/announcement.spec.ts
+++ src/test/components/Base/Card/announcement.spec.ts
@@ -7,22 +7,26 @@ describe('Announcementコンポーネントのテスト', () => {
     const wrapper = await mountSuspended(Announcement, {
       props: {
         content: '来週の授業は休校です',
-        dateTime: new Date().toISOString()
+        dateTime: new Date().toISOString(),
+        isEdited: false
       }
     })
 
     expect(wrapper.get(`[data-testId="timeAgo"]`).text()).toBe('今')
     expect(wrapper.get(`[data-testId="propsContent"]`).text()).toBe('来週の授業は休校です')
+    expect(wrapper.find(`[data-testId="propsIsEdited"]`).exists()).toBe(false)
   })
 
   it('dateTimeに1日前の日付を渡すと、「昨日」と表示される', async () => {
     const wrapper = await mountSuspended(Announcement, {
       props: {
         content: '来週の授業は休校です',
-        dateTime: new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString()
+        dateTime: new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString(),
+        isEdited: true
       }
     })
 
     expect(wrapper.get(`[data-testId="timeAgo"]`).text()).toBe('昨日')
+    expect(wrapper.find(`[data-testId="propsIsEdited"]`).exists()).toBe(true)
   })
 })

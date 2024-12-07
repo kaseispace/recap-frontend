2b698cda32403dc74582623de17a164c1fe51f00
yuki0829 <fledgling0829@gmail.com>
2024-12-07 10:14:35 +0900

    style: 最大幅を設定

--- src/components/Base/Snackbar/index.vue
+++ src/components/Base/Snackbar/index.vue
@@ -22,26 +22,29 @@ watch(
   <div
     v-if="isSnackbarVisible"
     data-testId="invisible"
-    class="fixed right-2 top-2 z-40"
+    class="fixed right-2 top-2 z-40 max-w-3/4"
   >
     <div
       data-testId="propsStatus"
-      class="flex items-center rounded-lg px-5 py-3"
+      class="flex items-center rounded-lg px-4 py-3"
       :class="[status ? 'bg-teal-900' : 'bg-red-500']"
     >
-      <Icon
-        v-if="!iconStyle"
-        name="mdi:alert-circle"
-        size="20px"
-        style="color: white"
-      />
-      <Icon
-        v-else
-        name="mdi:check-circle"
-        size="20px"
-        style="color: white"
-      />
-      <p class="ml-2 text-sm text-white">
+      <div class="flex items-center">
+        <Icon
+          v-if="!iconStyle"
+          name="mdi:alert-circle"
+          size="17px"
+          style="color: white"
+        />
+        <Icon
+          v-else
+          name="mdi:check-circle"
+          size="17px"
+          style="color: white"
+        />
+      </div>
+
+      <p class="ml-2 text-base text-white">
         {{ props.notification?.text }}
       </p>
     </div>

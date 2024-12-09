ec39054bdb71c56db86a71df54e9edf067bd4ad9
yuki0829 <fledgling0829@gmail.com>
2024-12-09 15:03:08 +0900

    chore: metaタグの変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,10 @@ export default defineNuxtConfig({
       title: '教員と学生のための振り返り学習プラットフォーム',
       titleTemplate: '%s - ReCap',
       meta: [
+        { name: 'description', content: 'ReCapは、教育現場での振り返りを支援するツールです。対話型の振り返りとフィードバック機能により、学びを深めるサポートをします。' },
         { property: 'og:title', content: '教員と学生のための振り返り学習プラットフォーム - ReCap' },
         { property: 'og:description',
-          content: 'ReCapは、教員と学生のための振り返り学習プラットフォームです。学びを深め、新たな気づきを得るためのサポートを提供します。' },
+          content: 'ReCapは、教育現場での振り返りを支援するツールです。対話型の振り返りとフィードバック機能により、学びを深めるサポートをします。' },
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: 'https://www.recaplearning.com' },
         { property: 'og:image', content: 'https://res.cloudinary.com/dbveryjmc/image/upload/v1733711476/OGP_zwixhe.png' },
@@ -23,7 +24,7 @@ export default defineNuxtConfig({
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:site', content: '@your_twitter_handle' },
         { name: 'twitter:title', content: '教員と学生のための振り返り学習プラットフォーム - ReCap' },
-        { name: 'twitter:description', content: 'ReCapは、教員と学生のための振り返り学習プラットフォームです。学びを深め、新たな気づきを得るためのサポートを提供します。' },
+        { name: 'twitter:description', content: 'ReCapは、教育現場での振り返りを支援するツールです。対話型の振り返りとフィードバック機能により、学びを深めるサポートをします。' },
         { name: 'twitter:image', content: 'https://res.cloudinary.com/dbveryjmc/image/upload/v1733711476/OGP_zwixhe.png' }
       ],
       link: [

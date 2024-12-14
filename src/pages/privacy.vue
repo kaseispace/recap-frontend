2a9f3c3d19828f048d323fcd4c562d4340185f2f
yuki0829 <fledgling0829@gmail.com>
2024-12-14 09:48:12 +0900

    feat: プライバシーポリシーの詳細な説明文を追加

--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -1,3 +1,110 @@
 <template>
-  <p>プライバシーポリシー</p>
+  <div class="flex justify-center">
+    <div class="flex max-w-[880px] flex-col items-center space-y-10 px-10">
+      <div>
+        <h1 class="section-header text-center text-2xl">
+          プライバシーポリシー
+        </h1>
+        <p class="small-gray-text pt-4">
+          本ウェブサイト上で提供するサービス「ReCap」（以下、「本サービス」といいます。）における、
+          ユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
+        </p>
+      </div>
+
+      <div>
+        <h2 class="section-header text-xl">
+          第1条（個人情報）
+        </h2>
+        <p class="small-gray-text pt-4">
+          「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、生存する個人に関する情報であって、
+          名前、メールアドレス、ユーザータイプ、所属学校など、特定の個人を識別できる情報（個人識別情報）を指します。
+        </p>
+      </div>
+
+      <div class="w-full">
+        <h2 class="section-header text-xl">
+          第2条（個人情報の収集方法）
+        </h2>
+        <p class="small-gray-text pt-4">
+          本サービスは、ユーザーが利用登録をする際に名前、メールアドレスなどの個人情報をお尋ねすることがあります。
+        </p>
+      </div>
+
+      <div>
+        <h2 class="section-header text-xl">
+          第3条（個人情報を収集・利用する目的）
+        </h2>
+        <p class="small-gray-text pt-4">
+          本サービスが個人情報を収集・利用する目的は、以下のとおりです。
+        </p>
+        <ol class="numbered-list space-y-2 pt-4">
+          <li>本サービスの提供・運営のため</li>
+          <li>ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
+          <li>メンテナンス、重要なお知らせなど必要に応じたご連絡のため</li>
+          <li>利用規約に違反したユーザーや、不正・不当な目的でサービスを利用しようとするユーザーの特定をし、ご利用をお断りするため</li>
+          <li>ユーザーにご自身の登録情報の閲覧や変更、削除、ご利用状況の閲覧を行っていただくため</li>
+          <li>上記の利用目的に付随する目的</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 class="section-header text-xl">
+          第4条（利用目的の変更）
+        </h2>
+        <ol class="numbered-list space-y-2 pt-4">
+          <li>本サービスは、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。</li>
+          <li>利用目的の変更を行った場合には、変更後の目的について、本サービス所定の方法により、ユーザーに通知し、または本ウェブサイト上に公表するものとします。</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 class="section-header text-xl">
+          第5条（個人情報の第三者提供）
+        </h2>
+        <p class="small-gray-text pt-4">
+          本サービスは、あらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。
+          ただし、個人情報保護法その他の法令で認められる場合を除きます。
+        </p>
+      </div>
+
+      <div>
+        <h2 class="section-header text-xl">
+          第6条（プライバシーポリシーの変更）
+        </h2>
+        <ol class="numbered-list space-y-2 pt-4">
+          <li>
+            本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく、変更することができるものとします。
+          </li>
+          <li>
+            本サービスが別途定める場合を除いて、変更後のプライバシーポリシーは、本ウェブサイトに掲載したときから効力を生じるものとします
+          </li>
+        </ol>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.section-header {
+  width: 100%;
+  border-bottom-width: 1.5px;
+  --tw-border-opacity: 1;
+  border-color: rgb(5 150 105 / var(--tw-border-opacity));
+  padding-bottom: 0.5rem;
+  font-weight: bold;
+}
+
+.numbered-list {
+  font-size: 15px;
+  --tw-text-opacity: 1;
+  color: rgb(17 24 39 / var(--tw-text-opacity));
+  list-style-type: decimal;
+  padding-left: 18px;
+}
+
+.small-gray-text {
+  font-size: 15px;
+  --tw-text-opacity: 1;
+  color: rgb(17 24 39 / var(--tw-text-opacity));
+}
+</style>

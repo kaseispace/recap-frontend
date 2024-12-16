251ce398e78e9b02494889b1ade471a47616ee7f
yuki0829 <fledgling0829@gmail.com>
2024-12-16 15:01:11 +0900

    test: パスワード再設定ページのテストを作成

---
+++ src/test/pages/forgotpassword.spec.ts
@@ -0,0 +1,31 @@
+// @vitest-environment nuxt
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import ForgotPasswordPage from '@/pages/forgotpassword.vue'
+import { wait } from '@/test/mocks/index'
+
+vi.mock('firebase/auth', () => ({
+  sendPasswordResetEmail: vi.fn()
+}))
+
+describe('forgotpasswordページのテスト', () => {
+  it('無効なメールアドレスでエラーメッセージが表示される', async () => {
+    const wrapper = await mountSuspended(ForgotPasswordPage)
+
+    await wrapper.find(`[data-testId="clickSend"]`).trigger('click')
+
+    await wait(10)
+
+    expect(wrapper.find(`[data-testId="errorEmail"]`).exists()).toBe(true)
+  })
+
+  it('パスワードリセットのためのメールが送信される', async () => {
+    const wrapper = await mountSuspended(ForgotPasswordPage)
+
+    await wrapper.get(`[data-testId="textInput"]`).setValue('test@example.com')
+    await wrapper.find(`[data-testId="clickSend"]`).trigger('click')
+
+    await wait(10)
+
+    expect(wrapper.find(`[data-testId="emailSent"]`).text()).toBe('再設定用URLを記載したメールを送信しました')
+  })
+})

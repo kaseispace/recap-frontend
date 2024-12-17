5e6ce1658bf3396818ea7c5545da20ff35ef7796
yuki0829 <fledgling0829@gmail.com>
2024-12-17 11:54:01 +0900

    refactor: MOCK_USER_SCHOOLを学生用と教員用に分割

--- src/test/mocks/user/index.ts
+++ src/test/mocks/user/index.ts
@@ -1,5 +1,10 @@
-export const MOCK_USER_SCHOOL = {
-  user: { id: 8, uid: 'h9I0j1K2l3M4n5O6p7Q8r9S0t1U2', name: '中村八郎', user_type: 0 },
+export const MOCK_STUDENT_USER_SCHOOL = {
+  user: { id: 1, uid: 'h9I0j1K2l3M4n5O6p7Q8r9S0t1U2', name: '中村八郎', user_type: 0 },
+  school: { id: 23, name: 'フルスタック大学' }
+}
+
+export const MOCK_TEACHER_USER_SCHOOL = {
+  user: { id: 2, uid: 'a1B2c3D4e5F6g7H8i9J0k1L2m3N4', name: '佐藤一郎', user_type: 1 },
   school: { id: 23, name: 'フルスタック大学' }
 }
 
@@ -11,8 +16,3 @@ export const MOCK_CURRENT_USER = [
     user_type: 0
   }
 ]
-
-export const MOCK_CURRENT_USER_SCHOOL = {
-  user: { id: 1, uid: 'A1B2C3D4E5F6G7H8I9J0K1L2M3N4', name: '山田太郎', user_type: 0 },
-  school: { id: 23, name: 'フルスタック大学' }
-}

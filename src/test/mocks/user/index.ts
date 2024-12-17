076a0b6856f49c55b8c0efd06c15b35b985daac8
yuki0829 <fledgling0829@gmail.com>
2024-12-17 11:04:02 +0900

    refactor: school名を変更

--- src/test/mocks/user/index.ts
+++ src/test/mocks/user/index.ts
@@ -1,6 +1,6 @@
 export const MOCK_USER_SCHOOL = {
   user: { id: 8, uid: 'h9I0j1K2l3M4n5O6p7Q8r9S0t1U2', name: '中村八郎', user_type: 0 },
-  school: { id: 23, name: '東京大学' }
+  school: { id: 23, name: 'フルスタック大学' }
 }
 
 export const MOCK_CURRENT_USER = [
@@ -14,5 +14,5 @@ export const MOCK_CURRENT_USER = [
 
 export const MOCK_CURRENT_USER_SCHOOL = {
   user: { id: 1, uid: 'A1B2C3D4E5F6G7H8I9J0K1L2M3N4', name: '山田太郎', user_type: 0 },
-  school: { id: 23, name: '東京大学' }
+  school: { id: 23, name: 'フルスタック大学' }
 }

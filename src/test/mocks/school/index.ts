4b4a73f1df0d2724cedbedb7822736dfe9272866
yuki0829 <fledgling0829@gmail.com>
2024-12-17 11:14:59 +0900

    refactor: MOCK_SCHOOLS定数の学校名を変更

--- src/test/mocks/school/index.ts
+++ src/test/mocks/school/index.ts
@@ -1,7 +1,7 @@
 export const MOCK_SCHOOLS = [
-  { id: 1, name: '東京大学' },
-  { id: 2, name: '京都大学' },
-  { id: 3, name: '大阪大学' },
-  { id: 4, name: '北海道大学' },
-  { id: 5, name: '九州大学' }
+  { id: 1, name: 'フルスタック大学' },
+  { id: 2, name: 'テクノロジー大学' },
+  { id: 3, name: 'イノベーション大学' },
+  { id: 4, name: 'アドバンス大学' },
+  { id: 5, name: 'クリエイティブ大学' }
 ]

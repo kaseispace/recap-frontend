2ae2080832ca1604a13dd08139c1e2cd7cc5a8e5
yuki0829 <fledgling0829@gmail.com>
2024-12-17 11:53:00 +0900

    refactor: MOCK_AUTH_USERをMOCK_STUDENT_AUTH_USERに変更

--- src/test/mocks/user/endpoints.ts
+++ src/test/mocks/user/endpoints.ts
@@ -1,15 +1,15 @@
 import { registerEndpoint } from '@nuxt/test-utils/runtime'
-import { MOCK_AUTH_USER } from '@/test/mocks/index'
+import { MOCK_STUDENT_AUTH_USER } from '@/test/mocks/index'
 import { MOCK_CURRENT_USER } from '@/test/mocks/user/index'
 
 const config = useRuntimeConfig()
 
 export const registerUserEndpoints = () => {
   // ユーザーの削除
-  registerEndpoint(`${config.public.backendUrl}/users/${MOCK_AUTH_USER.uid}`, {
+  registerEndpoint(`${config.public.backendUrl}/users/${MOCK_STUDENT_AUTH_USER.uid}`, {
     method: 'DELETE',
     handler: () => {
-      const deleteUser = MOCK_CURRENT_USER.find(user => user.uid === MOCK_AUTH_USER.uid)
+      const deleteUser = MOCK_CURRENT_USER.find(user => user.uid === MOCK_STUDENT_AUTH_USER.uid)
       if (deleteUser) {
         return {}
       }

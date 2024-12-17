643a362d80d774b310454cef42060d3454139d2e
yuki0829 <fledgling0829@gmail.com>
2024-12-17 20:31:49 +0900

    fix: エンドポイント削除処理で認証ユーザーのUIDを検証

--- src/test/mocks/user/endpoints.ts
+++ src/test/mocks/user/endpoints.ts
@@ -1,17 +1,18 @@
 import { registerEndpoint } from '@nuxt/test-utils/runtime'
 import { MOCK_STUDENT_AUTH_USER } from '@/test/mocks/index'
-import { MOCK_CURRENT_USER } from '@/test/mocks/user/index'
 
 const config = useRuntimeConfig()
 
 export const registerUserEndpoints = () => {
+  const auth = useAuth()
   // ユーザーの削除
   registerEndpoint(`${config.public.backendUrl}/users/${MOCK_STUDENT_AUTH_USER.uid}`, {
     method: 'DELETE',
     handler: () => {
-      const deleteUser = MOCK_CURRENT_USER.find(user => user.uid === MOCK_STUDENT_AUTH_USER.uid)
-      if (deleteUser) {
-        return {}
+      if (auth.authUser.value) {
+        if (auth.authUser.value.uid === MOCK_STUDENT_AUTH_USER.uid) {
+          return {}
+        }
       }
     }
   })

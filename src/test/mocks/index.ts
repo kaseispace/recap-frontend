e631bba4eafb7df9bb89cac30a7a3d50103acbde
yuki0829 <fledgling0829@gmail.com>
2024-12-17 11:52:24 +0900

    refactor: MOCK_AUTH_USERをMOCK_STUDENT_AUTH_USERとMOCK_TEACHER_AUTH_USERに分割

--- src/test/mocks/index.ts
+++ src/test/mocks/index.ts
@@ -1,12 +1,12 @@
 export const MOCK_EMPTY_UUID = 'empty-data-uuid-0000' // 空のデータが返されるときに使用
 export const MOCK_UUID = 'data-uuid-1111' // データが返されるときに使用
 export const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-export const MOCK_AUTH_USER = {
+export const MOCK_STUDENT_AUTH_USER = {
   delete: vi.fn(),
   displayName: null,
-  email: 'mockuser@example.com',
+  email: 'mockstudent@example.com',
   emailVerified: false,
-  getIdToken: vi.fn(() => Promise.resolve('mocked-idToken')),
+  getIdToken: vi.fn(() => Promise.resolve('mocked-student-idToken')),
   getIdTokenResult: vi.fn(),
   isAnonymous: false,
   metadata: {},
@@ -15,9 +15,9 @@ export const MOCK_AUTH_USER = {
   providerData: [
     {
       providerId: 'password',
-      uid: 'mockuser@example.com',
+      uid: 'mockstudentr@example.com',
       displayName: null,
-      email: 'mockuser@example.com',
+      email: 'mockstudent@example.com',
       phoneNumber: null,
       photoURL: null
     }
@@ -27,5 +27,34 @@ export const MOCK_AUTH_USER = {
   reload: vi.fn(),
   tenantId: 'tenant-id',
   toJSON: vi.fn(),
-  uid: 'A1B2C3D4E5F6G7H8I9J0K1L2M3N4'
+  uid: 'h9I0j1K2l3M4n5O6p7Q8r9S0t1U2'
+}
+
+export const MOCK_TEACHER_AUTH_USER = {
+  delete: vi.fn(),
+  displayName: null,
+  email: 'mockteacher@example.com',
+  emailVerified: false,
+  getIdToken: vi.fn(() => Promise.resolve('mocked-teacher-idToken')),
+  getIdTokenResult: vi.fn(),
+  isAnonymous: false,
+  metadata: {},
+  phoneNumber: null,
+  photoURL: null,
+  providerData: [
+    {
+      providerId: 'password',
+      uid: 'mockteacher@example.com',
+      displayName: null,
+      email: 'mockteacher@example.com',
+      phoneNumber: null,
+      photoURL: null
+    }
+  ],
+  providerId: 'firebase',
+  refreshToken: 'refresh-token',
+  reload: vi.fn(),
+  tenantId: 'tenant-id',
+  toJSON: vi.fn(),
+  uid: 'a1B2c3D4e5F6g7H8i9J0k1L2m3N4'
 }

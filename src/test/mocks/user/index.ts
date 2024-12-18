073a0c8b569f45c8c3125f6320c4a0f3333aa717
yuki0829 <fledgling0829@gmail.com>
2024-12-18 11:24:02 +0900

    refactor: モックデータの学校IDを修正

--- src/test/mocks/user/index.ts
+++ src/test/mocks/user/index.ts
@@ -1,11 +1,11 @@
 export const MOCK_STUDENT_USER_SCHOOL = {
   user: { id: 1, uid: 'h9I0j1K2l3M4n5O6p7Q8r9S0t1U2', name: '中村八郎', user_type: 0 },
-  school: { id: 23, name: 'フルスタック大学' }
+  school: { id: 1, name: 'フルスタック大学' }
 }
 
 export const MOCK_TEACHER_USER_SCHOOL = {
   user: { id: 2, uid: 'a1B2c3D4e5F6g7H8i9J0k1L2m3N4', name: '佐藤一郎', user_type: 1 },
-  school: { id: 23, name: 'フルスタック大学' }
+  school: { id: 1, name: 'フルスタック大学' }
 }
 
 export const MOCK_CURRENT_USER = [

4fcdae3ffa39bba374df5731da7dafe6b1a30202
yuki0829 <fledgling0829@gmail.com>
2025-05-14 20:57:25 +0900

    feat: Stepコンポーネントを作成

---
+++ src/components/Base/Flow/Step.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+interface Props {
+  timing: string
+  userType: string
+  stepNumber: string
+  imagePath: string
+  subtitle: string
+  subtitleAlt?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="w-full">
+    <div class="flex flex-col gap-y-4">
+      <div class="flex w-full items-center justify-center bg-[#057767] p-2">
+        <p class="text-center text-base font-bold text-white">
+          {{ timing }}<br>
+          ({{ userType }})
+        </p>
+      </div>
+
+      <div class="flex h-[160px] w-full flex-col items-center justify-center gap-y-1 bg-white p-2">
+        <p class="text-base font-bold text-[#057767]">
+          {{ stepNumber }}
+        </p>
+
+        <div class="flex h-full flex-col items-center justify-center gap-y-1">
+          <NuxtImg
+            format="webp"
+            provider="cloudinary"
+            :src="imagePath"
+            width="75px"
+          />
+
+          <h4 class="text-center text-base font-bold">
+            {{ subtitle }}<br>
+            {{ subtitleAlt }}
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

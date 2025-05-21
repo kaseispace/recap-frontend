ac8912279f64b23a83066b131000922dd2e25230
yuki0829 <fledgling0829@gmail.com>
2025-05-21 16:13:21 +0900

    feat: AnnouncementCardコンポーネントを作成

---
+++ src/components/Base/Skeleton/AnnouncementCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mb-4 w-full rounded-md border border-gray-300 shadow-md">
+    <div class="flex animate-pulse flex-col p-3">
+      <div class="mb-3 h-4 w-12 rounded-full bg-gray-300" />
+
+      <div class="grid gap-y-2">
+        <div class="h-3 rounded-full bg-gray-300" />
+        <div class="h-3 w-1/2 rounded-full bg-gray-300" />
+        <div class="h-3 w-4/5 rounded-full bg-gray-300" />
+      </div>
+    </div>
+  </div>
+</template>

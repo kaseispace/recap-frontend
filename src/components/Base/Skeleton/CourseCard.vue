85ed37834e8a2b0dc8d9d0261c61166c49c66bc7
yuki0829 <fledgling0829@gmail.com>
2025-05-21 10:17:35 +0900

    feat: CourseCardコンポーネントを作成

---
+++ src/components/Base/Skeleton/CourseCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full rounded-md border shadow-md">
+    <div class="flex animate-pulse flex-col">
+      <div class="h-32 rounded-t-md border border-gray-300 bg-gray-300" />
+      <div class="h-10 p-3">
+        <div class="grid grid-cols-12 gap-3">
+          <div class="col-span-6 h-4 rounded-full bg-gray-300 opacity-0" />
+          <div class="col-span-3 h-4 rounded-full bg-gray-300" />
+          <div class="col-span-3 h-4 rounded-full bg-gray-300" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

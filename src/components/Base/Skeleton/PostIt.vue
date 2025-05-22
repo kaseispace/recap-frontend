5708d997d51088bbf7ea8854a6a7272e6cb7e87d
yuki0829 <fledgling0829@gmail.com>
2025-05-22 11:19:50 +0900

    feat: PostItコンポーネントを作成

---
+++ src/components/Base/Skeleton/PostIt.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative w-full max-w-96 border shadow">
+    <div class="flex animate-pulse flex-col p-3">
+      <div
+        class="absolute -top-3 left-1/2 h-8 w-20 -translate-x-1/2 rotate-3 bg-gray-300"
+      />
+
+      <div class="mt-5 grid gap-y-3">
+        <div class="h-5 rounded-full bg-gray-300" />
+        <div class="h-5 rounded-full bg-gray-300" />
+        <div class="h-5 w-1/3 rounded-full bg-gray-300" />
+        <div class="h-5 w-4/5 rounded-full bg-gray-300" />
+        <div class="h-5 rounded-full bg-gray-300" />
+        <div class="h-5 w-1/2 rounded-full bg-gray-300" />
+      </div>
+    </div>
+  </div>
+</template>

c4eed6aaa5c0d3bbf7f0172512d19a7a44bcb77b
yuki0829 <fledgling0829@gmail.com>
2025-05-22 10:40:05 +0900

    feat: ReflectionHistoryコンポーネントを作成

---
+++ src/components/Base/Skeleton/ReflectionHistory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="w-full rounded-lg border border-gray-300 p-5 shadow">
+    <div class="flex animate-pulse flex-col">
+      <div class="mb-4 border-b border-gray-300 pb-3">
+        <div class="mb-1 h-7 w-1/3 rounded-full bg-gray-300" />
+        <div class="h-5 w-1/5 rounded-full bg-gray-300" />
+      </div>
+
+      <div class="mb-4 h-24 rounded-md bg-gray-300" />
+
+      <div class="space-y-3">
+        <div class="flex items-center">
+          <div class="size-10 rounded-full bg-gray-300" />
+          <div class="ml-3 h-6 w-1/3 rounded-full bg-gray-300" />
+        </div>
+
+        <div class="flex items-center">
+          <div class="size-10 rounded-full bg-gray-300" />
+          <div class="ml-3 h-6 w-1/2 rounded-full bg-gray-300" />
+        </div>
+
+        <div class="flex items-center">
+          <div class="size-10 rounded-full bg-gray-300" />
+          <div class="ml-3 h-6 w-full rounded-full bg-gray-300" />
+        </div>
+
+        <div class="flex items-center">
+          <div class="size-10 rounded-full bg-gray-300" />
+          <div class="ml-3 h-6 w-4/5 rounded-full bg-gray-300" />
+        </div>
+
+        <div class="flex items-center">
+          <div class="size-10 rounded-full bg-gray-300" />
+          <div class="ml-3 h-6 w-3/5 rounded-full bg-gray-300" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

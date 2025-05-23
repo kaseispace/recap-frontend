771cccb36b21170e38a95a80e558d9724a32a297
yuki0829 <fledgling0829@gmail.com>
2025-05-23 10:36:50 +0900

    feat: PromptCardコンポーネントを作成

---
+++ src/components/Base/Skeleton/PromptCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full rounded-md border shadow-md">
+    <div class="flex animate-pulse flex-col">
+      <div class="h-[52px] rounded-t-md bg-gray-300" />
+
+      <div class="px-5 pb-2 pt-5">
+        <div
+          v-for="n in 3"
+          :key="n"
+          class="grid gap-y-2 pb-3"
+        >
+          <div class="h-3 rounded-full bg-gray-300" />
+          <div class="h-3 w-2/3 rounded-full bg-gray-300" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
